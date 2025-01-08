23f3a790b8b96022fbd3d64feb065c1d0fd25d2c
Gulalov Mahammad <72382783+GMahammad@users.noreply.github.com>
2025-01-08 11:19:20 +0400

    Add files via upload

---
+++ New-E-taxes.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         New-E-taxes
+// @namespace    http://tampermonkey.net/
+// @version      0.0.3
+// @description  Automatically fetch and display tax data.
+// @run-at       document-start
+// @author       Mahammad Gulalov
+// @match        https://new.e-taxes.gov.az/*
+// @exclude      https://new.e-taxes.gov.az/eportal/az/invoice/view/*
+// @updateURL    https://raw.githubusercontent.com/GMahammad/E-Taxes/main/New-E-taxes.meta.js
+// @downloadURL  https://raw.githubusercontent.com/GMahammad/E-Taxes/main/New-E-taxes.js
+// @icon         https://qaime.e-taxes.gov.az/assets/images/placeholder.jpg
+// @connect
