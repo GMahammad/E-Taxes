0a71d654b3b1a51c5f052388888e3dc3ec9c8ca2
Gulalov Mahammad <72382783+GMahammad@users.noreply.github.com>
2025-01-08 11:21:15 +0400

    Update New-E-taxes.meta.js

--- New-E-taxes.meta.js
+++ New-E-taxes.meta.js
@@ -5,9 +5,10 @@
 // @description  Automatically fetch and display tax data.
 // @run-at       document-start
 // @author       Mahammad Gulalov
-// @match        https://new.e-taxes.gov.az/*
-// @exclude      https://new.e-taxes.gov.az/eportal/az/invoice/view/*
 // @updateURL    https://raw.githubusercontent.com/GMahammad/E-Taxes/main/New-E-taxes.meta.js
 // @downloadURL  https://raw.githubusercontent.com/GMahammad/E-Taxes/main/New-E-taxes.js
+// @match        https://new.e-taxes.gov.az/*
+// @exclude      https://new.e-taxes.gov.az/eportal/az/invoice/view/*
 // @icon         https://qaime.e-taxes.gov.az/assets/images/placeholder.jpg
 // @connect
+// ==UserScript==
